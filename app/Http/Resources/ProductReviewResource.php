19f2d6989549af7fe27f5b60eb5ab1d867b751fd
turain <biswajit.turaindev@gmail.com>
2026-04-18 16:02:04 +0530

    Mapped product added successfully

--- app/Http/Resources/ProductReviewResource.php
+++ app/Http/Resources/ProductReviewResource.php
@@ -79,6 +79,84 @@ class ProductReviewResource extends JsonResource
                 $this->whenLoaded('productAttributes')
             ),
 
+            'mapped_products' => collect($this->mapped_products ?? [])->map(function ($product) {
+
+                return [
+                    'id' => Hashids::encode($product->id),
+                    'business_id' => Hashids::encode($product->business_id),
+                    'business_category_id' => Hashids::encode($product->business_category_id),
+
+                    'name' => $product->name,
+                    'sku' => $product->sku,
+                    'description' => $product->description,
+
+                    'mrp' => $product->mrp,
+                    'cost_price' => $product->cost_price,
+                    'selling_price' => $product->selling_price,
+                    'discount' => $product->discount,
+                    'final_price' => $product->final_price,
+
+                    'manufacture_date' => $product->manufacture_date,
+                    'expiry_date' => $product->expiry_date,
+
+                    'image' => $product->image
+                        ? asset('storage/' . $product->image)
+                        : null,
+
+                    'status' => $product->status,
+
+                    // FIXED: correct status label
+                    'status_label' => match ($product->status) {
+                        1 => 'Approved',
+                        2 => 'Unapproved',
+                        default => 'Unknown',
+                    },
+
+                    // OPTIONAL: only if relations exist on product model
+                    'business_category' => isset($product->businessCategory) ? [
+                        'id' => Hashids::encode($product->businessCategory->id),
+                        'name' => $product->businessCategory->name,
+                    ] : null,
+
+                    'business_sub_category' => isset($product->businessSubCategory) ? [
+                        'id' => Hashids::encode($product->businessSubCategory->id),
+                        'name' => $product->businessSubCategory->name,
+                    ] : null,
+
+                    'product_category' => isset($product->category) ? [
+                        'id' => Hashids::encode($product->category->id),
+                        'name' => $product->category->name,
+                    ] : null,
+
+                    'product_sub_category' => isset($product->subCategory) ? [
+                        'id' => Hashids::encode($product->subCategory->id),
+                        'name' => $product->subCategory->name,
+                    ] : null,
+
+                    'product_sub_sub_category' => isset($product->subSubCategory) ? [
+                        'id' => Hashids::encode($product->subSubCategory->id),
+                        'name' => $product->subSubCategory->name,
+                    ] : null,
+
+                    'hsn' => isset($product->hsn) ? [
+                        'id' => Hashids::encode($product->hsn->id),
+                        'code' => $product->hsn->hsn_code,
+                    ] : null,
+
+                    // Attributes encoding
+                    'attributes' => collect($product->attributes ?? [])->map(function ($attr) {
+                        return [
+                            'id' => Hashids::encode($attr->id),
+                            'attribute_id' => Hashids::encode($attr->attribute_id),
+                            'attribute_value_id' => Hashids::encode($attr->attribute_value_id),
+
+                            'stock' => $attr->stock,
+                            'price' => $attr->price,
+                        ];
+                    }),
+                ];
+            }),
+
             // Timestamp
             'created_at' => $this->created_at,
         ];
