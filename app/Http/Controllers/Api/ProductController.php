755358afe36a216a00295567b254649887881950
turain <biswajit.turaindev@gmail.com>
2026-05-27 11:18:00 +0530

    Product update remove variation_status date to integer

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -690,7 +690,7 @@ class ProductController extends Controller
                 'variants.*.final_price' => 'nullable|numeric',
                 'variants.*.stock' => 'nullable|integer',
                 'variants.*.manufacture_date' => 'nullable|date',
-                'variants.*.variant_status' => 'nullable|date',
+                'variants.*.variant_status' => 'nullable|integer',
                 'variants.*.expiry_date' => 'nullable|date',
 
                 'variants.*.short_description' => 'nullable|string|max:1000',
