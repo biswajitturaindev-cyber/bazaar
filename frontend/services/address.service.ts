185ba12a39df262fffb187ac950b2f9a99612b61
turain <biswajit.turaindev@gmail.com>
2026-07-10 16:55:26 +0530

    Remove Frontend

--- frontend/services/address.service.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/lib/axios";
-import { UserAddressListResponse } from "@/types/address";
-
-export const getUserAddresses = async (): Promise<UserAddressListResponse> => {
-    const { data } = await api.get<UserAddressListResponse>("/member/v1/user-addresses");
-    return data;
-};
